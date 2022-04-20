cb81f21542e1e8fc03b36b4164d58ad497605e4e
Eldersantos17! <san17004@byui.edu>
2022-04-19 20:36:41 -0600

    C# practice

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+string first;
+string last;
+
+Console.WriteLine("what is your first name?");
+first = Console.ReadLine();
+
+Console.WriteLine("what is your last name?");
+last = Console.ReadLine();
+
+Console.WriteLine($"Your name is {last}, {first} {last}.");
+        }
+    }
+}
+
